70af67cbb7889649f6daeafe176d30a227922198
taekim90 <taehk15@gmail.com>
2022-05-24 13:34:38 -0700

    creation of routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db")
 
 // middleware
 app.use(cors());
 
 app.use(express.json())
+
+// ROUTES
+
+// create a todo
+app.post("/todos", async (req,res) => {
+    // await
+    try {
+        console.log(req.body)
+    } catch (error) {
+        // console.error(error.message)
+        console.log(error)
+    }
+})
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
 
 
 app.listen(8000, () => {
