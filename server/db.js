add21d3c548788c1177e040ac8803e4c0c357d8c
taekim90 <taehk15@gmail.com>
2022-05-27 13:31:14 -0700

    attempting to fix post route

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user:"postgres",
-    // password: ""
+    password: "",
     host:"localhost",
     port: 3000,
     database: "perntodo"
